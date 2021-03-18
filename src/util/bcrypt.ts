1492301d14c80b59a5340ee396b04d9d9af03f34
octavioduarte <octavioduarte2105@gmail.com>
2021-03-18 18:03:39 -0300

    feat: create class to encrypt password

---
+++ src/util/bcrypt.ts
@@ -0,0 +1,13 @@
+import bcrypt from 'bcrypt'
+
+export class BcryptAdapter {
+  constructor (private readonly salt: number) {}
+
+  async hash (plaintext: string): Promise<string> {
+    return bcrypt.hash(plaintext, this.salt)
+  }
+
+  async compare (plaintext: string, digest: string): Promise<boolean> {
+    return bcrypt.compare(plaintext, digest)
+  }
+}

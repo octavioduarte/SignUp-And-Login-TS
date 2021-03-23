40b0d21b832c16fd7f9fb04b20d1744a81de1df5
octavioduarte <octavioduarte2105@gmail.com>
2021-03-23 04:22:22 -0300

    refactor: include typeof on validation RequiredFieldValidation

--- src/validators/required-field-validation.ts
+++ src/validators/required-field-validation.ts
@@ -5,7 +5,7 @@ export class RequiredFieldValidation implements Validation {
     constructor(private readonly fieldName: string) { }
 
     validate(input: any): Error | undefined {
-        if (!input[this.fieldName]) {
+        if (!input[this.fieldName] && typeof input[this.fieldName] !== 'boolean') {
             return new MissingParamError(this.fieldName)
         }
     }
